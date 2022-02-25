076c2238f7bf5cd682995a6d0d98ca1ed810a939
AXSharp <artis.stpens@gmail.com>
2022-02-25 17:34:25 +0200

    commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -47,26 +47,26 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
-  capabilities: [
-    {
-    browserName: 'chrome',
-    browserName: 'firefox'
-    }
-    ],
-    // capabilities: [{
+//   capabilities: [
+//     {
+//     browserName: 'chrome'
+//     //browserName: 'firefox'
+//     }
+//     ],
+    capabilities: [{
 
-    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    //     // grid with only 5 firefox instances available you can make sure that not more than
-    //     // 5 instances get started at a time.
-    //     maxInstances: 5,
-    //     //
-    //     browserName: 'chrome',
-    //     acceptInsecureCerts: true
-    //     // If outputDir is provided WebdriverIO can capture driver session logs
-    //     // it is possible to configure which logTypes to include/exclude.
-    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-    //     // excludeDriverLogs: ['bugreport', 'server'],
-    // }],
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 5,
+        //
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+        // If outputDir is provided WebdriverIO can capture driver session logs
+        // it is possible to configure which logTypes to include/exclude.
+        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        // excludeDriverLogs: ['bugreport', 'server'],
+    }],
   //
   // ===================
   // Test Configurations
@@ -114,15 +114,15 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [
-    ['selenium-standalone', {
-        driver: {
-            chrome: 'latest',
-            firefox: 'latest'
-        }
-    }]
-],
-//services: ['chromedriver','devtools'],
+//   services: [
+//     ['selenium-standalone', {
+//         driver: {
+//             chrome: 'latest',
+//             firefox: 'latest'
+//         }
+//     }]
+// ],
+services: ['chromedriver','devtools'],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
